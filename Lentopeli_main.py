f59be53e748073f3d5067cbbbc943b70a6d27fb3
luarakelly <lua.kellly@gmail.com>
2025-09-18 15:23:12 +0300

    Base starter for the flight game.

--- Lentopeli_main.py
+++ Lentopeli_main.py
@@ -1,2 +1,31 @@
 import mysql.connector
 import random
+
+# ===  Constants ====
+
+# === Function: automatically defining stage criterias === 
+def task_criteria():
+    # 0 = Max CO2 consumpition, 1= flights, 2= countries
+    stages_starter =  (1000, 5, 3) # Atleast 5 stages
+    return 
+
+# ==== Function: Distace counter ====
+
+# ==== Function: table creator ====
+
+# === Function: ask user input ====
+def user_input (question):
+    return input(question)
+
+# === Function: Allow user to pass next or keep same if fail task ====
+def pass_stage():
+    return
+
+# === Main program ===
+welcome_message = ""
+print(welcome_message)
+user_name = user_input('Give your name: ')
+(n, task) = task_criteria()
+Task = f"You are currently in Finland Helsinki, this is your {n} task: {task}"
+print("choose your destination (country and city)")
+country_selection = user_input(' Choose a country: ')
