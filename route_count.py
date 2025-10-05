c7a9d78d13a87e924af55672b305c25b1a950b9b
Maria Kuznetsova <makuza.mk@gmail.com>
2025-10-05 13:53:00 +0300

    feat: modify map,  add airport search functionality

--- route_count.py
+++ route_count.py
@@ -1,6 +1,7 @@
 from geopy import distance
 import folium
 import os
+from folium.plugins import Search
 
 # ==== Function: Get all airports ====
 def get_all_airports(yhteys):
@@ -148,17 +149,28 @@ def showMap(coordinates, output_file='map.html'):
         print(f"Map '{output_file}' already exists. Skipping creation.")
         return
     map = folium.Map(location=[coordinates[0]['lat'], coordinates[0]['lng']], zoom_start=5)
-    folium.Marker(
-        location=[coordinates[0]['lat'], coordinates[0]['lng']],
-        popup=coordinates[0]['name'],
-        icon=folium.Icon(color='green', icon='info-sign')
-    ).add_to(map)
-    print(f"Adding {len(coordinates)} markers to the map...")
+    marker_group = folium.FeatureGroup(name="Airports")
 
-    for i in range(1, len(coordinates)):
-        folium.Marker(
-            location=[coordinates[i]['lat'], coordinates[i]['lng']],
-            popup=f"{coordinates[i]['name']}<br><b>{coordinates[i]['ident']}</b><br>{coordinates[i]['city']}, {coordinates[i]['country']}",
-        ).add_to(map)
+    for airport in coordinates:
+        marker = folium.Marker(
+            location=[airport['lat'], airport['lng']],
+            popup=f"{airport['name']}<br><b>{airport['ident']}</b><br>{airport['city']}, {airport['country']}",
+            tooltip=airport['ident'],  # search by name or ICAO code
+            icon=folium.Icon(color='blue', icon='plane', prefix='fa')
+        )
+
+        marker.options['name'] = f"{airport['ident']} - {airport['name']} - {airport['city']}"
+        marker.add_to(marker_group)
+
+    marker_group.add_to(map)
+
+    Search(
+        layer=marker_group,
+        search_label='name',
+        placeholder='Search by name, city or ICAO code',
+        collapsed=False,
+        search_zoom=7,
+        casesensitive=False
+    ).add_to(map)
 
     map.save('map.html')
