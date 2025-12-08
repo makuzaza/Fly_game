e46d3bbbbaef1ae7255ba81a5dfb0b074214f09c
luarakelly <lua.kellly@gmail.com>
2025-12-08 15:28:17 +0200

    Upper developed the user unput validation.

--- front_end/chatHelpers.js
+++ front_end/chatHelpers.js
@@ -4,7 +4,11 @@
 // -----------------------------------
 // USER MSG INPUT VALIDATOR
 // -----------------------------------
-export function validateCountryInput(code) {
+export function validateCountryInput(code, places) {
+    //const stage = get_game_status();
+    if (!places) {
+      return { valid: false, message: "Game data missing. Reload the game." };
+    }
     //Must not be empty
     if (!code) {
       return{ valid: false, message: "Type a country code." };
@@ -18,14 +22,15 @@ export function validateCountryInput(code) {
     if (!/^[A-Z]{2}$/.test(code)) {
       return { valid: false, message: "Country code must contain only letters." };
     }
-
-    // Check if the ISO code exists in the stage's 'places'
-    if (!stage.places || !stage.places[code]) {
-      return { valid: false, message: `X ${code} is not one of the target countries for this stage.` };
+    
+    console.log('places[code]: ', places[code]);
+    if (!Object.keys(places).includes(code)) { //   GET THE KEY       
+        return { valid: false, message: `X ${code} is not one of the target countries for this stage.` };
     }
 
     // Lookup ICAO code for this country
-    const icao = stage.places[code];
+    const icao = places[code];
+    console.log('icao: ', icao);
 
     return {
         valid: true,
@@ -33,7 +38,7 @@ export function validateCountryInput(code) {
         icao: icao,
         message: `Correct guess: ${code} -> Airport ${icao}`
     };
-  }
+}
 
 // -----------------------------------
 //  FIRST STAGE INTRO MESSAGES
