8416c5654f7b9558bcf476ccf88357b925b3577a
Maria Kuznetsova <makuza.mk@gmail.com>
2025-12-09 10:44:25 +0200

    feat: push updates

--- back_end/lentopeli_api.py
+++ back_end/lentopeli_api.py
@@ -470,4 +470,5 @@ def create_app():
 # Run
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="localhost", port=5000, debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
