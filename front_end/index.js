d93684d7a880d45b32f605879da1a027e447ff9d
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2025-12-09 19:01:59 +0200

    Deleted commented lines

--- front_end/index.js
+++ front_end/index.js
@@ -234,11 +234,6 @@ async function showGameScreen() {
     <div id="map-container">
         <!-- FOOTER: 3 tips -->
         <footer id="tips-footer" aria-label="Tips"></footer>
-        <!--<footer id="tips-footer" aria-label="Tips">
-            <div class="tip" id="tip1"><span class="tip-label">Tip 1:</span> unguessed</div>
-            <div class="tip highlighted" id="tip2"><span class="tip-label">Tip 2:</span> guessed</div>
-            <div class="tip" id="tip3"><span class="tip-label">Tip 3:</span> unguessed</div>
-        </footer>-->
     </div>
 
     <div class="side-panel">
