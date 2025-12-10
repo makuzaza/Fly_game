8b8fdaeba29fb6d98081f4e4774b3b21591f0763
Maria Kuznetsova <makuza.mk@gmail.com>
2025-12-10 14:17:31 +0200

    Add railway server

--- front_end/app.js
+++ front_end/app.js
@@ -28,7 +28,7 @@ let gameState = {
   backupTotal: null,
 };
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://flygame-production.up.railway.app";
 let mapInitialized = false;
 
 function getAirportInfo(ident) {
