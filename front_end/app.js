0294e48dba534376e7a79d1c71aa2ea051e81e39
Maria K <makuza.mk@gmail.com>
2026-01-25 15:43:33 +0200

    update api_url

--- front_end/app.js
+++ front_end/app.js
@@ -33,8 +33,13 @@ let gameState = {
   currentStops: 0, 
 };
 
-// const API_URL = "http://localhost:5000";
-const API_URL = "https://flygame-production.up.railway.app";
+const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+  ? 'http://localhost:5000'
+  : 'https://flygame-production.up.railway.app';
+
+// -----------------------------
+// Map Initialization Flag
+// -----------------------------
 let mapInitialized = false;
 
 function getAirportInfo(ident) {
